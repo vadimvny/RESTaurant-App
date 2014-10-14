537943793f44603fe86ac96a23c42091c8e118a8
Poopsik <vadimvny@Vadims-MacBook-Pro.local>
2014-10-14 12:14:14 -0400

    created database called restaurant_db

--- Gemfile
+++ Gemfile
@@ -3,6 +3,6 @@ source 'http://rubygems.org'
 gem 'pry'
 gem 'pg'
 gem 'sinatra'
-gem 'sinatra-activereader'
+gem 'sinatra-activerecord'
 gem 'sinatra-reloader'
 
---
+++ restaurant_db.rb
@@ -0,0 +1,25 @@
+require 'sinatra/activerecord'
+require 'pg'
+require 'rainbow'
+
+    # As an employee, I want to be able to add new food items... so the menu can change
+    # As an employee, I want to be able to edit previously added food items... so I can update descriptions
+    # As an employee, I want to be able to delete previously added food items... so I can remove bad food
+    # As an employee, I want to be able to add food items to a party as they order them... so I can keep track of a party's orders
+    # As an employee, I want to be able to create a receipt ... so I can print it
+
+conn = PG::Connection.open()
+
+conn.exec('CREATE DATABASE restaurant_db;')
+conn.exec('CREATE TABLE foods (id SERIAL PRIMARY KEY, name VARCHAR(255), description VARCHAR(255), price integer;')
+conn.exec('CREATE TABLE parties (id SERIAL PRIMARY KEY, size integer, reservation_time integer;')
+conn.exec('CREATE TABLE orders (id SERIAL PRIMARY KEY, food_id VARCHAR(255), parties_id VARCHAR(255);')
+conn.close
+
+
+ActiveRecord::Base.establish_connection({
+
+	adapter: 'postgresql'
+	database: 'restaurant_db'
+
+	})
