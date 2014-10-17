6db1d3165ebb1a8d9e7bb66adaf611204f079f27
Poopsik <vadimvny@Vadims-MacBook-Pro.local>
2014-10-17 08:55:00 -0400

    Holds all the classes Party, Order, and Food

--- models/food.rb
+++ models/food.rb
@@ -1,12 +1,10 @@
 class Food < ActiveRecord::Base
 	has_many(:orders)
 	has_many(:parties, :through => :orders)
-	validates :name, uniqueness: true, length: {in: 2..15}
+	validates :name, uniqueness: true, length: {in: 1..15}
 	validates :description, uniqueness: true 
+	
 	def to_s
-		"Name: #{name}  Description: #{description} Price: #{price}" 
-	end
-	def total 
-		
+		"Name: #{food.name}  Description: #{food.description} Price: #{food.price}" 
 	end
 end
--- models/party.rb
+++ models/party.rb
@@ -2,3 +2,10 @@ class Party < ActiveRecord::Base
 	has_many(:orders)
 	has_many(:foods, :through => :orders)
 end
+
+def counter (count)
+	count = 0
+	while count < 15
+		count += 1
+	end
+end
