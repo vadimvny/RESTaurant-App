f74489f15a4e3e37b813cd495ec4715125d8f583
Poopsik <vadimvny@Vadims-MacBook-Pro.local>
2014-11-01 19:56:50 -0400

    created users_controller with get and post routes to create new users

---
+++ controllers/user_controller.rb
@@ -0,0 +1,12 @@
+class UserController < ApplicationController
+  get '/new' do
+    erb :'users/new'
+  end
+
+  post '/' do
+    user = User.new(params[:user])
+    user.password = params[:password]
+    user.save!
+    redirect '/sessions/new'
+  end
+end
